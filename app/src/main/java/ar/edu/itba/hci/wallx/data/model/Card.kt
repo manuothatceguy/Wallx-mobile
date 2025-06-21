8444ccbda90fd2f02d0f05a3e091668d4fd0593d
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2025-06-21 19:26:10 -0300

    fix

--- app/src/main/java/ar/edu/itba/hci/wallx/data/model/Card.kt
+++ app/src/main/java/ar/edu/itba/hci/wallx/data/model/Card.kt
@@ -1,12 +1,10 @@
 package ar.edu.itba.hci.wallx.data.model
 
-import java.util.Date
-
 data class Card(
     var id: Int,
     var type: String,
     var number: String,
-    var expirationDate: Date,
+    var expirationDate: String,
     var fullName: String
 ) {
 }
