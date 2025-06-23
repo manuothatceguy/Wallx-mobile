45692fa8e07b72624717f5890f24c5a5916eed5d
manuothatceguy <141188032+manuothatceguy@users.noreply.github.com>
2025-06-23 14:32:24 -0300

    Refactor: Increase minSdk to 26
    The minimum SDK version for the application has been increased from 24 to 26 in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "ar.edu.itba.hci.wallx"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
